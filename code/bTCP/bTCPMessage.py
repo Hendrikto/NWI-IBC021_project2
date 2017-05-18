bcae17699d94ea03164eb29e5bb5fec25d9299cb
Hendrikto <hendrik.to@gmail.com>
2017-05-18 17:06:10 +0200

    Add a bTCP message.

---
+++ code/bTCP/bTCPMessage.py
@@ -0,0 +1,23 @@
+# author: Hendrik Werner s4549775
+# author: Constantin Blach s4329872
+import struct
+
+import zlib
+
+from bTCP.bTCPHeader import BTCPHeader
+
+
+class BTCPMessage(object):
+    def __init__(
+            self,
+            header: BTCPHeader,
+            payload: bytes,
+    ):
+        self.header = header
+        self.payload = payload
+
+    def to_bytes(self):
+        header_bytes = self.header.to_bytes()
+        return header_bytes + struct.pack("L", zlib.crc32(
+            header_bytes + self.payload
+        )) + self.payload
