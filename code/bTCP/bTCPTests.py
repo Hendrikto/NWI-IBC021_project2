3da37e8e54f0ed307b93ba1d42608cf4a11eb869
Hendrikto <hendrik.to@gmail.com>
2017-05-19 01:07:45 +0200

    Test header serialization.

--- code/bTCP/bTCPTests.py
+++ code/bTCP/bTCPTests.py
@@ -4,6 +4,14 @@ from bTCP.bTCPMessage import BTCPMessage
 from bTCP.bTCPHeader import BTCPHeader
 
 
+class BTCPHeaderTest(unittest.TestCase):
+    def test_serialization(self):
+        self.assertEqual(
+            BTCPHeader(1, 2, 3, 4, 5, 6).to_bytes(),
+            b"\x00\x00\x00\x01\x00\x02\x00\x03\x04\x05\x00\x06"
+        )
+
+
 class BTCPMessageTest(unittest.TestCase):
     def test_padding(self):
         header = BTCPHeader(1, 2, 3, 4, 5, 6)
