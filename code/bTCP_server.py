2b3870af5e41d963cfae6efe43222831396e8d0e
ConstantinB <c.blach@student.ru.nl>
2017-05-21 16:21:11 +0200

    Add commandline args for IP and port

--- code/bTCP_server.py
+++ code/bTCP_server.py
@@ -16,15 +16,21 @@ parser.add_argument(
     "-o", "--output", help="Where to store file", default="tmp.file"
 )
 args = parser.parse_args()
+parser.add_argument(
+    "-s", "--serverip", help="Define server IP", type=str,
+    default="127.0.0.1"
+)
+parser.add_argument(
+    "-p", "--serverport", help="Define server port", type=int,
+    default=9001
+)
 
-server_ip = "127.0.0.1"
-server_port = 9001
 
 # Define a header format
 header_format = "I"
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # UDP
-sock.bind((server_ip, server_port))
+sock.bind((args.serverip, args.serverport))
 
 while True:
     data, addr = sock.recvfrom(1016)
